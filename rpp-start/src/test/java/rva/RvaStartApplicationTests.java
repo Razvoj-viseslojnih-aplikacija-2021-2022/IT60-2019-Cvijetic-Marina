f1c8e58ead527a8190842baee694dedfb9a037d4
marinacvijetic <marinacvijetic2@gmail.com>
2022-05-18 23:26:19 +0200

    /

--- rpp-start/src/test/java/rva/RvaStartApplicationTests.java
+++ rpp-start/src/test/java/rva/RvaStartApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RppStartApplicationTests {
+class RvaStartApplicationTests {
 
 	@Test
 	void contextLoads() {
