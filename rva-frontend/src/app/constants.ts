7c40daefc75a7e44661474ded31a0944f7c70b67
marinacvijetic <marinacvijetic2@gmail.com>
2022-08-23 00:16:04 +0200

    Completed frontend

---
+++ rva-frontend/src/app/constants.ts
@@ -0,0 +1,5 @@
+export const OBRAZOVANJE_URI = "http://localhost:8083/obrazovanje";
+export const PREDUZECE_URI = "http://localhost:8083/preduzece";
+export const SEKTOR_URI = "http://localhost:8083/sektor";
+export const RADNIK_URI = "http://localhost:8083/radnik";
+export const RADNIK_BY_SEKTOR_URI = "http://localhost:8083/radnik/bySektor";
