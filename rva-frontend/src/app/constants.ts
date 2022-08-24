cf4a55b9e1c71663b9f587c8508b580b3151863b
marinacvijetic <marinacvijetic2@gmail.com>
2022-08-24 03:41:20 +0200

    Update constants.ts

--- rva-frontend/src/app/constants.ts
+++ rva-frontend/src/app/constants.ts
@@ -4,8 +4,8 @@
 // export const RADNIK_URI = "http://localhost:8083/radnik";
 // export const RADNIK_BY_SEKTOR_URI = "http://localhost:8083/radnik/bySektor";
 
-export const OBRAZOVANJE_URI = "http://backend-application2022.herokuapp.com/obrazovanje";
-export const PREDUZECE_URI = "http://backend-application2022.herokuapp.com/preduzece";
-export const SEKTOR_URI = "http://backend-application2022.herokuapp.com/sektor";
-export const RADNIK_URI = "http://backend-application2022.herokuapp.com/radnik";
-export const RADNIK_BY_SEKTOR_URI = "http://backend-application2022.herokuapp.com/radnik/bySektor";
+export const OBRAZOVANJE_URI = "http://backendmarina.herokuapp.com/obrazovanje";
+export const PREDUZECE_URI = "http://backendmarina.herokuapp.com/preduzece";
+export const SEKTOR_URI = "http://backendmarina.herokuapp.com/sektor";
+export const RADNIK_URI = "http://backendmarina.herokuapp.com/radnik";
+export const RADNIK_BY_SEKTOR_URI = "http://backendmarina.herokuapp.com/radnik/bySektor";
